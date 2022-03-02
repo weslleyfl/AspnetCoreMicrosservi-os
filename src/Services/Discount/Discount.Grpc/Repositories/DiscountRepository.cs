af02273b4dc2a3b7149b7b882403a67427ccad0f
Weslley Lopes <oweslley@outlook.com>
2022-03-02 17:52:14 -0300

    Finalizando seçao 6 - Discount Grpc Microservices

---
+++ src/Services/Discount/Discount.Grpc/Repositories/DiscountRepository.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dapper;
+using Discount.Grpc.Entities;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace Discount.Grpc.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public async Task<Coupon> GetDiscountAsync(string productName)
+        {
+            using var connection = new NpgsqlConnection
+               (_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+
+            var query = "SELECT * FROM Coupon WHERE ProductName ILIKE CONCAT('%',@ProductName,'%');";
+
+            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>(
+                sql: query,
+                param: new
+                {
+                    ProductName = productName
+                });
+
+            if (coupon == null)
+            {
+                return new Coupon
+                {
+                    ProductName = "No Discount",
+                    Amount = 0,
+                    Description = "No Discount Description"
+                };
+            }
+
+            return coupon;
+
+        }
+
+        public async Task<bool> CreateDiscountAsync(Coupon coupon)
+        {
+            using var connection = new NpgsqlConnection
+               (_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+
+            var affected = await connection.ExecuteAsync(
+                sql: "INSERT INTO Coupon (ProductName, Description, Amount) VALUES (@ProductName, @Description, @Amount)",
+                param: new
+                {
+                    ProductName = coupon.ProductName,
+                    Description = coupon.Description,
+                    Amount = coupon.Amount
+                });
+
+            return affected != 0;
+        }
+
+        public async Task<bool> DeleteDiscountAsync(string productName)
+        {
+            using var connection = new NpgsqlConnection
+               (_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+
+            var affected = await connection.ExecuteAsync(
+                sql: "DELETE FROM Coupon WHERE ProductName = @ProductName",
+                 param: new
+                 {
+                     ProductName = productName
+                 });
+
+
+            return affected != 0;
+        }
+
+
+        public async Task<bool> UpdateDiscountAsync(Coupon coupon)
+        {
+            using var connection = new NpgsqlConnection
+               (_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+
+            var affected = await connection.ExecuteAsync(
+                sql: "UPDATE Coupon SET ProductName=@ProductName, Description = @Description, Amount = @Amount WHERE Id = @Id",
+                 param: new
+                 {
+                     ProductName = coupon.ProductName,
+                     Description = coupon.Description,
+                     Amount = coupon.Amount,
+                     Id = coupon.Id
+
+                 });
+
+            return affected != 0;
+
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Repositories/IDiscountRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discount.Grpc.Entities;
+
+namespace Discount.Grpc.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscountAsync(string productName);
+        Task<bool> CreateDiscountAsync(Coupon coupon);
+        Task<bool> UpdateDiscountAsync(Coupon coupon);
+        Task<bool> DeleteDiscountAsync(string productName);
+
+    }
+}
